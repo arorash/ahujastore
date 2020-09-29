33180e9f396bdc9745cbfc98e83aa2137e882ec2
arorash <56073547+arorash@users.noreply.github.com>
2020-09-29 16:18:08 +0530

    result in proper format

--- getproducts.php
+++ getproducts.php
@@ -1,18 +1,17 @@
 <?php
     require_once dirname(__FILE__).'/DbOperation.php';
     $response = array();
-    #if($_SERVER['REQUEST_METHOD']=='POST'){
-     #   if(isset($_POST['username'])){
-
-      #  }
-
-    #}
-    $db = new DbOperation();
-            $products = $db->getItemsId();
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        if(isset($_POST['username'])){
+            $db = new DbOperation();
+            $products = $db->getItems();
             for($i=0;$i<count($products);$i++){
-                array_push($response,$products[$i]['id']);
+                array_push($response,$products[$i]);
             }
+            echo json_encode($response);
+        }
 
-    for($i=0;$i<count($products);$i++){
-        echo $response[$i];
+    }
+    else{
+        echo "Invalid Request";
     }
