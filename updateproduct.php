980a00e90a0004265c97f403cce68d0afb51cb24
arorash <56073547+arorash@users.noreply.github.com>
2020-10-08 10:25:03 +0530

    final update

--- updateproduct.php
+++ updateproduct.php
@@ -1,9 +1,17 @@
 <?php
     require_once dirname(__FILE__).'/DbOperation.php';
     $response = array();
-    /*if($_SERVER['REQUEST_METHOD']=='POST'){
+    if($_SERVER['REQUEST_METHOD']=='POST'){
         if(isset($_POST['product_name']) and isset($_POST['price']) and isset($_POST['quantity'])){
             $db = new DbOperation();
+    if($db->updatePro($_POST['product_name'],$_POST['price'],$_POST['quantity'])){
+        $response['error']=false;
+        $response['message']="Save Successfully";
+    }
+    else{
+        $response['error']=true;
+        $response['message']="Some Error Occur";
+    }
 
         }
         else{
@@ -14,14 +22,7 @@
     else{
         $response['error']=true;
         $response['message']="Invalid Request.";
-    }*/
-    if($db->updatePro("PARLE,GOODDAY","5,10","10,5")){
-        $response['error']=false;
-        $response['message']="Save Successfully";
     }
-    else{
-        $response['error']=true;
-        $response['message']="Some Error Occur";
-    }
+
 
     echo json_encode($response);
